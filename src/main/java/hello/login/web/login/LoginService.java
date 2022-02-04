be1825539513eee99f19c4a447d50bad07e70218
wlwlwon <ryanwon@naver.com>
2022-02-05 00:01:03 +0900

    login

---
+++ src/main/java/hello/login/web/login/LoginService.java
@@ -0,0 +1,19 @@
+package hello.login.web.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLonginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
